505d180d5e305b05a8b392589bd75fd4766fa469
Adel Stiti <40324627+adelstiti@users.noreply.github.com>
2019-04-14 21:50:50 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,68 +1,38 @@
-// Write CPP code here 
 #include <netdb.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
 #include <sys/socket.h> 
 #include <arpa/inet.h>
-
 #define MAX 80 
 #define PORT 8080 
 #define SA struct sockaddr 
-void func(int sockfd) 
-{ 
-	char buff[MAX]; 
-	int n; 
-	for (;;) { 
-		bzero(buff, sizeof(buff)); 
-		printf("Enter the string : "); 
-		n = 0; 
-		while ((buff[n++] = getchar()) != '\n') 
-			; 
-		write(sockfd, buff, sizeof(buff)); 
-		bzero(buff, sizeof(buff)); 
-		read(sockfd, buff, sizeof(buff)); 
-		printf("From Server : %s", buff); 
-		if ((strncmp(buff, "exit", 4)) == 0) { 
-			printf("Client Exit...\n"); 
-			break; 
-		} 
-	} 
-} 
-
 int main() 
 { 
-	int sockfd, connfd; 
+	int sockfd, connfd;
 	struct sockaddr_in servaddr, cli; 
 
-	// socket create and varification 
+	// creation et verification du socket 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 	if (sockfd == -1) { 
-		printf("socket creation failed...\n"); 
+		printf("création du socket échoué...\n"); 
 		exit(0); 
 	} 
 	else
-		printf("Socket successfully created..\n"); 
+	printf("Socket créer avec success..\n"); 
 	bzero(&servaddr, sizeof(servaddr)); 
-
-	// assign IP, PORT 
+	// IP et PORT du serveur
 	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	servaddr.sin_addr.s_addr = inet_addr("192.168.43.217"); 
 	servaddr.sin_port = htons(PORT); 
 
-	// connect the client socket to server socket 
+	// connection au serveur 
 	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
-		printf("connection with the server failed...\n"); 
+		printf("connection au serveur échoué...\n"); 
 		exit(0); 
 	} 
 	else
-		printf("connected to the server..\n"); 
-
-	/* function for chat 
-	func(sockfd); 
-	printf("IP of Client : %s \n ",inet_ntoa(cli.sin_addr));
-		*/
-	// close the socket 
+		printf("connecté au serveur ..\n"); 
 	close(sockfd); 
 } 
 
